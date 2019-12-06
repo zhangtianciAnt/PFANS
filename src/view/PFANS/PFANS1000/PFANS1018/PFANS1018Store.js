08146ff9375a81cb21f711a934fd2def2a1f37d5
Administrator <280883874@qq.com>
2019-12-06 14:36:26 +0800

    1018

---
+++ src/view/PFANS/PFANS1000/PFANS1018/PFANS1018Store.js
@@ -0,0 +1,82 @@
+import {
+  getglobal,
+  getglobalApplyOne,
+  updateglobalApply,
+  createglobalApply,
+  getglobalApplyList
+} from './PFANS1018Api'
+
+const PFANS1018Store = {
+  namespaced: true,
+  state: {},
+  mutations: {},
+  actions: {
+    getglobal() {
+      return new Promise((resolve, reject) => {
+        getglobal().then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+    getglobalApplyOne({ commit },data) {
+      return new Promise((resolve, reject) => {
+        getglobalApplyOne(data).then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+    updateglobalApply({ commit },data) {
+      return new Promise((resolve, reject) => {
+        updateglobalApply(data).then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+    createglobalApply({ commit },data) {
+      return new Promise((resolve, reject) => {
+        createglobalApply(data).then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+    getglobalApplyList({ commit },data) {
+      return new Promise((resolve, reject) => {
+        getglobalApplyList(data).then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+  }
+};
+
+export default PFANS1018Store;
