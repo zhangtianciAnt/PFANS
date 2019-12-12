62865b40148cdc2bdaa4a86b9e551d775a7625e0
费家亮 <jialiang.fei@newtouch.com>
2019-12-12 15:58:26 +0800

    添加服务器地址画面

---
+++ src/view/ASSETS/ASSETS1000/ASSETS1003/ASSETS1003View.vue
@@ -0,0 +1,78 @@
+<template>
+  <div style="min-height: 100%">
+    <EasyNormalContainer :buttonList="buttonList" v-loading="loading" :title="title"
+                         ref="container">
+      <div slot="customize">
+        <el-form :model="form" label-position="left" label-width="8rem" ref="ruleForm"
+                 style="padding: 2rem">
+          <el-row>
+            <el-col :span="24">
+              <el-form-item :label="$t('label.ASSETS1003VIEW_FILEADDRESS')" prop="fileaddress">
+                <el-input v-model="form.fileaddress" @change="getAddre"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="24" align="center">
+              <div id="qrcode"></div>
+           </el-col>
+          </el-row>
+        </el-form>
+      </div>
+    </EasyNormalContainer>
+  </div>
+</template>
+
+<script>
+  import EasyNormalContainer from '@/components/EasyNormalContainer';
+  import {Message} from 'element-ui';
+  import QRCode from 'qrcodejs2';
+
+  export default {
+    name: 'ASSETS1003View',
+    components: {
+      EasyNormalContainer,
+    },
+    data() {
+      return {
+        loading: false,
+        title: 'title.ASSETS1003VIEW',
+        buttonList: [],
+        form: {
+          fileaddress: '',
+        },
+        qrcodeC: '',
+      };
+    },
+    mounted() {
+    },
+    created() {
+    },
+    methods: {
+      getAddre(){
+        // if(this.qrcodeC !== ''){
+        //   this.$('#qrcode').html("");
+        // }
+         this.qrcodeC = new QRCode('qrcode', {
+          width: 132,
+          height: 132,
+          text: this.form.fileaddress, // 二维码地址
+          colorDark: "#000",
+          colorLight: "#fff",
+        })
+      },
+    },
+  };
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+  #qrcode {
+    display: inline-block;
+    img {
+      width: 132px;
+      height: 132px;
+      background-color: #fff; //设置白色背景色
+      padding: 6px; // 利用padding的特性，挤出白边
+    }
+  }
+</style>
