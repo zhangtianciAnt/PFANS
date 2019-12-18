7dd297cc50306ec47ccfc91c543e35ab8aa4667c
Administrator <280883874@qq.com>
2019-12-18 14:59:37 +0800

    Merge branch 'DEV' of F:\PFANS\PFANS with conflicts.

--- src/view/PFANS/PFANS2000/PFANS2010/PFANS2010FormView.vue
+++ src/view/PFANS/PFANS2000/PFANS2010/PFANS2010FormView.vue
@@ -29,196 +29,189 @@
                 data: [],
                 rowid: '',
                 row_id: 'attendance_id',
-                form: {
-                    attendanceid: '',
-                    recognitionstate: '1',
-                },
                 columns: [
                     {
                         code: 'dates',
                         label: 'label.PFANS2010VIEW_APPLICATION',
-                        labelClass: 'pfans2010view_column_1',
-                        width: 110,
+                        labelClass: 'pfans2010view_column_17',
+                        width: 65,
                         fix: false,
                         filter: true,
                     },
                     {
                         code: 'SERVICE',
                         label: 'label.PFANS2010VIEW_SERVICE',
-                        labelClass: 'pfans2010view_column_2',
+                        labelClass: 'pfans2010view_column_5',
                         child: [
                             {
                                 code: 'normal',
                                 label: 'label.PFANS2010VIEW_NORMAL',
-                                labelClass: 'pfans2010view_column_3',
-                                width: 150,
+                                labelClass: 'pfans2010view_column_8',
+                                width: 90,
                                 fix: false,
                                 filter: true,
                             }, {
                                 code: 'ordinaryindustry',
                                 label: 'label.PFANS2010VIEW_OVERTIME',
-                                labelClass: 'pfans2010view_column_3',
-                                width: 150,
+                                labelClass: 'pfans2010view_column_8',
+                                width: 120,
                                 fix: false,
                                 filter: true,
                             }, {
                                 code: 'ordinaryindustrynight',
                                 label: 'label.PFANS2010VIEW_NIGHTOVERTIME',
-                                labelClass: 'pfans2010view_column_3',
+                                labelClass: 'pfans2010view_column_8',
                                 width: 150,
                                 fix: false,
                                 filter: true,
                             }, {
                                 code: 'weekendindustry',
                                 label: 'label.PFANS2010VIEW_RETIREMENT',
-                                labelClass: 'pfans2010view_column_3',
-                                width: 150,
+                                labelClass: 'pfans2010view_column_8',
+                                width: 120,
                                 fix: false,
                                 filter: true,
                             }, {
                                 code: 'weekendindustrynight',
                                 label: 'label.PFANS2010VIEW_NIGHTRETIREMENT',
-                                labelClass: 'pfans2010view_column_3',
-                                width: 90,
+                                labelClass: 'pfans2010view_column_8',
+                                width: 150,
                                 fix: false,
                                 filter: true,
                             }, {
                                 code: 'statutoryresidue',
                                 label: 'label.PFANS2010VIEW_HOLIDAYS',
-                                labelClass: 'pfans2010view_column_3',
-                                width: 150,
+                                labelClass: 'pfans2010view_column_8',
+                                width: 160,
                                 fix: false,
                                 filter: true,
                             }, {
                                 code: 'statutoryresiduenight',
                                 label: 'label.PFANS2010VIEW_NIGHTHOLIDAYS',
-                                labelClass: 'pfans2010view_column_3',
-                                width: 110,
+                                labelClass: 'pfans2010view_column_8',
+                                width: 190,
                                 fix: false,
                                 filter: true,
                             }, {
                                 code: 'annualrestday',
                                 label: 'label.PFANS2010VIEW_EVERYYEAR',
-                                labelClass: 'pfans2010view_column_3',
-                                width: 150,
+                                labelClass: 'pfans2010view_column_8',
+                                width: 160,
                                 fix: false,
                                 filter: true,
                             }, {
                                 code: 'specialday',
                                 label: 'label.PFANS2010VIEW_OCCASIONS',
-                                labelClass: 'pfans2010view_column_3',
-                                width: 150,
+                                labelClass: 'pfans2010view_column_8',
+                                width: 180,
                                 fix: false,
                                 filter: true,
                             }, {
                                 code: 'youthday',
                                 label: 'label.PFANS2010VIEW_YOUTHDAY',
-                                labelClass: 'pfans2010view_column_3',
-                                width: 150,
+                                labelClass: 'pfans2010view_column_8',
+                                width: 130,
                                 fix: false,
                                 filter: true,
                             }, {
                                 code: 'womensday',
                                 label: 'label.PFANS2010VIEW_WOMENSDAY',
-                                labelClass: 'pfans2010view_column_3',
-                                width: 150,
+                                labelClass: 'pfans2010view_column_8',
+                                width: 130,
                                 fix: false,
                                 filter: true,
                             },
                         ],
-                        width: 1650,
                     },
                     {
                         code: 'SICKLEAVE',
                         label: 'label.PFANS2010VIEW_SICKLEAVE',
-                        labelClass: 'pfans2010view_column_4',
+                        labelClass: 'pfans2010view_column_9',
                         child: [
                             {
                                 code: 'shortsickleave',
                                 label: 'label.PFANS2010VIEW_SHORT',
-                                labelClass: 'pfans2010view_column_5',
-                                width: 150,
+                                labelClass: 'pfans2010view_column_10',
+                                width: 105,
                                 fix: false,
                                 filter: true,
                             },
                             {
                                 code: 'longsickleave',
                                 label: 'label.PFANS2010VIEW_LONG',
-                                labelClass: 'pfans2010view_column_5',
-                                width: 150,
+                                labelClass: 'pfans2010view_column_10',
+                                width: 105,
                                 fix: false,
                                 filter: true,
                             },
                         ],
-                        width: 2100,
                     },
                     {
                         code: 'compassionateleave',
                         label: 'label.PFANS2010VIEW_LEAVE',
-                        labelClass: 'pfans2010view_column_6',
-                        width: 80,
+                        labelClass: 'pfans2010view_column_11',
+                        width: 40,
                     },
                     {
                         code: 'annualrest',
                         label: 'label.PFANS2010VIEW_INHUGH',
-                        labelClass: 'pfans2010view_column_6',
-                        width: 80,
+                        labelClass: 'pfans2010view_column_11',
+                        width: 40,
                     },
                     {
                         code: 'daixiu',
                         label: 'label.PFANS2010VIEW_DAYOFF',
-                        labelClass: 'pfans2010view_column_6',
-                        width: 80,
+                        labelClass: 'pfans2010view_column_11',
+                        width: 40,
                     },
                     {
                         code: 'nursingleave',
                         label: 'label.PFANS2010VIEW_MATERNITY',
-                        labelClass: 'pfans2010view_column_6',
-                        width: 80,
+                        labelClass: 'pfans2010view_column_11',
+                        width: 70,
                     },
                     {
                         code: 'welfare',
                         label: 'label.PFANS2010VIEW_WELFARE',
-                        labelClass: 'pfans2010view_column_6',
-                        width: 80,
+                        labelClass: 'pfans2010view_column_11',
+                        width: 60,
                     },
                     {
                         code: '',
                         label: 'label.PFANS2010VIEW_ABSENCE',
-                        labelClass: 'pfans2010view_column_7',
+                        labelClass: 'pfans2010view_column_14',
                         child: [
                             {
                                 code: 'late',
                                 label: 'label.PFANS2010VIEW_LATE',
-                                labelClass: 'pfans2010view_column_8',
-                                width: 150,
+                                labelClass: 'pfans2010view_column_15',
+                                width: 90,
                                 fix: false,
                                 filter: true,
                             },
                             {
                                 code: 'leaveearly',
                                 label: 'label.PFANS2010VIEW_LEAVEEARLY',
-                                labelClass: 'pfans2010view_column_8',
-                                width: 150,
+                                labelClass: 'pfans2010view_column_15',
+                                width: 90,
                                 fix: false,
                                 filter: true,
                             },
                             {
                                 code: 'absenteeism',
                                 label: 'label.PFANS2010VIEW_ABSENTEEISM',
-                                labelClass: 'pfans2010view_column_8',
-                                width: 150,
+                                labelClass: 'pfans2010view_column_15',
+                                width: 120,
                                 fix: false,
                                 filter: true,
                             },
                         ],
-                        width: 450,
                     },
                     {
                         code: 'recognitionstate',
                         label: 'label.PFANS2010VIEW_RECOGNITION',
-                        labelClass: 'pfans2010view_column_9',
-                        width: 150,
+                        labelClass: 'pfans2010view_column_16',
+                        width: 80,
                         fix: false,
                         filter: true,
                     },
@@ -231,8 +224,7 @@
         },
         methods: {
             rowClick(row) {
-                this.rowid = row.attendanceid;
-                this.form.attendanceid = row.attendanceid;
+                this.rowid = row.attendance_id;
             },
             buttonClick(val) {
                 this.$store.commit('global/SET_HISTORYURL', this.$route.path)
@@ -242,35 +234,20 @@
                     });
                 }
                 else if (val === 'recognition') {
-                    if (this.rowid === '') {
-                        Message({
-                            message: this.$t('normal.info_01'),
-                            type: 'error',
-                            duration: 2 * 1000
-                        });
-                        return;
-                    }
-                    this.loading = true;
-                    this.$store
-                        .dispatch('PFANS2010Store/update', this.form)
-                        .then(response => {
-                            this.data = response;
-                            this.getAttendancelist();
-                            this.loading = false;
-                            Message({
-                                message: this.$t('normal.success_02'),
-                                type: 'success',
-                                duration: 5 * 1000,
-                            });
-                        })
-                        .catch(error => {
-                            Message({
-                                message: error,
-                                type: 'error',
-                                duration: 5 * 1000
-                            });
-                            this.loading = false;
-                        })
+                    // this.loading = true;
+                    // this.$store
+                    //     .dispatch('PFANS2010Store/download', {})
+                    //     .then(response => {
+                    //         this.loading = false;
+                    //     })
+                    //     .catch(error => {
+                    //         Message({
+                    //             message: error,
+                    //             type: 'error',
+                    //             duration: 5 * 1000
+                    //         });
+                    //         this.loading = false;
+                    //     })
                 }
             },
             getAttendancelist() {
@@ -317,44 +294,55 @@
 </script>
 
 <style lang="scss" rel="stylesheet/scss">
-  /*.pfans2010view_column_1 {*/
-  /*  background: #425E72;*/
-  /*  color: #ffffff;*/
-  /*}*/
-  .pfans2010view_column_2 {
-    /*background: #5CBFA3;*/
-    /*color: #ffffff;*/
-    text-align: center;
+  .pfans2010view_column_5 {
+    height: 40px;
+    background: #5CBFA3;
+    color: #ffffff;
   }
-  /*.pfans2010view_column_3 {*/
-  /*  background: #AEDFD1;*/
-  /*  color: #ffffff;*/
-  /*}*/
-  .pfans2010view_column_4 {
-    /*background: #2696C3;*/
-    /*color: #ffffff;*/
-    text-align: center;
+
+  .pfans2010view_column_8 {
+    height: 40px;
+    background: #AEDFD1;
+    color: #ffffff;
   }
-  /*.pfans2010view_column_5 {*/
-  /*  background: #93CBE1;*/
-  /*  color: #ffffff;*/
-  /*}*/
-  /*.pfans2010view_column_6 {*/
-  /*  background: #F2BC6A;*/
-  /*  color: #ffffff;*/
-  /*}*/
-  .pfans2010view_column_7 {
-    /*background: #E5575E;*/
-    /*color: #ffffff;*/
-    text-align: center;
+  .pfans2010view_column_9 {
+    height: 40px;
+    background: rgb(38,150,195);
+    color: #ffffff;
   }
-  /*.pfans2010view_column_8 {*/
-  /*  background: #F2ABAF;*/
-  /*  color: #ffffff;*/
-  /*}*/
-  /*.pfans2010view_column_9 {*/
-  /*  background: #CCCCCC;*/
-  /*  color: #ffffff;*/
-  /*}*/
+  .pfans2010view_column_10 {
+    height: 40px;
+    background: rgb(147,203,225);
+    color: #ffffff;
+  }
+  .pfans2010view_column_11 {
+    height: 80px;
+    background: #F2BC6A;
+    color: #ffffff;
+  }
+
+  .pfans2010view_column_14 {
+    height: 40px;
+    background: #E5575E;
+    color: #ffffff;
+  }
+
+  .pfans2010view_column_15 {
+    height: 40px;
+    background: #F2ABAF;
+    color: #ffffff;
+  }
+
+  .pfans2010view_column_16 {
+    height: 81px;
+    background: #CCCCCC;
+    color: #ffffff;
+  }
+
+  .pfans2010view_column_17 {
+    height:81px;
+    background: #425E72;
+    color: #ffffff;
+  }
 </style>
 
