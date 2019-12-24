347b75c437656501ed5010099cd5e6e8367ff89b
赵国栋 <guodong.zhao@newtouch.cn>
2019-12-24 10:42:02 +0800

    1025

---
+++ src/view/PFANS/PFANS1000/PFANS1025/PFANS1025Store.js
@@ -0,0 +1,42 @@
+import {
+  get,
+  update,
+} from './PFANS1025Api'
+
+const PFANS1025Store = {
+  namespaced: true,
+  state: {},
+  mutations: {},
+  actions: {
+    get() {
+      return new Promise((resolve, reject) => {
+        get().then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+
+    update({commit}, data) {
+      return new Promise((resolve, reject) => {
+        update(data).then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+
+  }
+};
+
+export default PFANS1025Store;
