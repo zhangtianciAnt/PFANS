ee28759756f7eacf7cb226c6421e784f12a2f72d
杜松 <553468334@qq.com>
2019-12-26 18:05:56 +0800

    2022

--- src/view/PFANS/PFANS1000/PFANS1026/PFANS1026Store.js
+++ src/view/PFANS/PFANS1000/PFANS1026/PFANS1026Store.js
@@ -23,19 +23,6 @@ import {
           })
         })
       },
-      selectById({ commit },data) {
-        return new Promise((resolve, reject) => {
-          selectById(data).then(response => {
-            if (response.code === 0) {
-              resolve(response.data);
-            } else {
-              reject(response.message)
-            }
-          }).catch(error => {
-            reject(error);
-          })
-        })
-      },
       update({ commit },data) {
         return new Promise((resolve, reject) => {
           update(data).then(response => {
