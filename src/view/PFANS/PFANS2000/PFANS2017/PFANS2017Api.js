983edfce11af86a502d2dbfe3a49b930a7e6a072
赵国栋 <guodong.zhao@newtouch.cn>
2019-12-26 18:44:35 +0800

    修改

--- src/view/PFANS/PFANS2000/PFANS2017/PFANS2017Api.js
+++ src/view/PFANS/PFANS2000/PFANS2017/PFANS2017Api.js
@@ -1,11 +1,20 @@
 import request from "../../../../utils/request"
-
+import requestDownload from '../../../../utils/requestDownload'
 
 
 export function getFpans2017List() {
   return request({
     url: 'punchcardrecord/list',
     method: 'post'
+  })
+}
+
+export function download(data) {
+  return requestDownload({
+    url: 'punchcardrecord/download',
+    method: 'post',
+    data: data,
+    responseType: 'blob'
   })
 }
 
