f9ce779554bdcd91e822532a7c9f4632ea3f0888
Administrator <280883874@qq.com>
2020-01-14 14:25:46 +0800

    ztc

---
+++ src/view/PFANS/PFANS6000/PFANS6006/PFANS6006Api.js
@@ -0,0 +1,33 @@
+import request from '../../../../utils/request'
+
+export function getvariousfunds(data) {
+  return request({
+    url: 'variousfunds/get',
+    method: 'get',
+    params: data
+  })
+}
+
+export function getvariousfundsApplyOne(data) {
+  return request({
+    url: 'variousfunds/one',
+    method: 'post',
+    data: data
+  })
+}
+
+export function updatevariousfundsApply(data) {
+  return request({
+    url: 'variousfunds/update',
+    method: 'post',
+    data: data
+  })
+}
+
+export function createvariousfundsApply(data) {
+  return request({
+    url: 'variousfunds/create',
+    method: 'post',
+    data: data
+  })
+}
