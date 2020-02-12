4dd5ce06b247bc5d0826d69824fcc73f8169e8c2
影 <ying.zhang7@newtouch.cn>
2020-02-12 15:16:46 +0800

    ----by zy BP管理-费用统计

---
+++ src/view/PFANS/PFANS6000/PFANS6008/PFANS6008Api.js
@@ -0,0 +1,11 @@
+import request from '../../../../utils/request'
+
+// export function getvariousfunds(data) {
+//   return request({
+//     url: 'variousfunds/get',
+//     method: 'get',
+//     params: data
+//   })
+// }
+
+
---
+++ src/view/PFANS/PFANS6000/PFANS6008/PFANS6008Store.js
@@ -0,0 +1,22 @@
+import {} from './PFANS6008Api'
+const PFANS6008Store = {
+  namespaced: true,
+  state: {},
+  mutations: {},
+  actions: {
+    // getvariousfunds() {
+    //   return new Promise((resolve, reject) => {
+    //     getvariousfunds().then(response => {
+    //       if (response.code === 0) {
+    //         resolve(response.data);
+    //       } else {
+    //         reject(response.message)
+    //       }
+    //     }).catch(error => {
+    //       reject(error);
+    //     })
+    //   })
+    // }
+};
+
+export default PFANS6008Store;
