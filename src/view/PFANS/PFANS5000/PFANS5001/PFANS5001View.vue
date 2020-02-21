ced1f87332ea00236c18ccef3fce6128c38c2ac9
Mr.Wang <wxz0407225>
2020-02-21 11:08:55 +0800

    起案書画面

--- src/view/PFANS/PFANS5000/PFANS5001/PFANS5001View.vue
+++ src/view/PFANS/PFANS5000/PFANS5001/PFANS5001View.vue
@@ -18,7 +18,7 @@
         data() {
             return {
                 loading: false,
-                title: "title.PFANS5001VIEW",
+                title: "menu.PFANS5001",
                 // 表格数据源
                 data: [],
                 // 列属性
