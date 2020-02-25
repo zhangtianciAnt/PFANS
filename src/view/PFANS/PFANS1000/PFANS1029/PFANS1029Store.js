18fbcf1141dc518d40b66cc9e868374c1be9b912
zyb <2458086357@qq.com>
2020-02-25 13:20:11 +0800

    zyb

---
+++ src/view/PFANS/PFANS1000/PFANS1029/PFANS1029Store.js
@@ -0,0 +1,55 @@
+import {
+  get,
+  one,
+  update
+} from './PFANS1029Api'
+
+const PFANS1029Store = {
+  namespaced: true,
+  state: {},
+  mutations: {},
+  actions: {
+    get() {
+      return new Promise((resolve, reject) => {
+        get().then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+
+    one({commit}, data) {
+      return new Promise((resolve, reject) => {
+        one(data).then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+
+    update({commit}, data) {
+      return new Promise((resolve, reject) => {
+        update(data).then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+  }
+}
+export default PFANS1029Store;
