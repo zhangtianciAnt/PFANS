fc6c45706319fc1361ca9e2561d6ee0499ecbc58
赵国栋 <guodong.zhao@newtouch.cn>
2020-02-26 11:42:04 +0800

    修改表格控件

--- src/components/EasyMenu/vertical.vue
+++ src/components/EasyMenu/vertical.vue
@@ -6,35 +6,35 @@
           <i :class="ob.menuicon" v-if="ob.menuicon"></i>
           <span>{{$t(ob.name)}}</span>
         </template>
-        <el-menu-item-group v-for="obi in ob.children" v-if="obi && obi.children && obi.children.length > 0 && Object.keys(obi.children[0]).length > 0" :index="obi._id" :key="obi._id" v-show="obi.menuvisible">
+        <el-submenu v-for="obi in ob.children" v-if="obi && obi.children && obi.children.length > 0 && Object.keys(obi.children[0]).length > 0" :index="obi._id" :key="obi._id" v-show="obi.menuvisible">
           <template slot="title">
             <i :class="obi.menuicon" v-if="obi.menuicon"></i>
             <span>{{$t(obi.name)}}</span>
           </template>
 
-          <el-menu-item-group v-for="cobi in obi.children" v-if="cobi && cobi.children && cobi.children.length > 0 && Object.keys(cobi.children[0]).length > 0" :index="cobi._id" :key="cobi._id" v-show="cobi.menuvisible">
+          <el-submenu v-for="cobi in obi.children" v-if="cobi && cobi.children && cobi.children.length > 0 && Object.keys(cobi.children[0]).length > 0" :index="cobi._id" :key="cobi._id" v-show="cobi.menuvisible">
             <template slot="title">
               <i :class="cobi.menuicon" v-if="cobi.menuicon"></i>
               <span>{{$t(cobi.name)}}</span>
             </template>
-            <el-menu-item v-for="ccobi in cobi.children" v-if="ccobi.menuvisible" :key="ccobi._id" :index="ccobi.menuurl" v-show="ccobi.menuvisible">
-              <i :class="ccobi.menuicon" v-if="ccobi.menuicon"></i>
-              <span>{{$t(ccobi.name)}}</span>
+            <el-menu-item v-for="cobii in cobi.children" v-if="cobii.menuvisible" :key="cobii._id" :index="cobii.menuurl" v-show="cobii.menuvisible">
+              <span>{{$t(cobii.name)}}</span>
+              <i class="el-icon-right"></i>
             </el-menu-item>
-          </el-menu-item-group>
+          </el-submenu>
           <el-menu-item v-else :key="cobi._id" :index="cobi.menuurl" v-show="cobi.menuvisible">
-            <i :class="cobi.menuicon" v-if="cobi.menuicon"></i>
             <span>{{$t(cobi.name)}}</span>
+            <i class="el-icon-right"></i>
           </el-menu-item>
-        </el-menu-item-group>
+        </el-submenu>
         <el-menu-item v-else :key="obi._id" :index="obi.menuurl" v-show="obi.menuvisible">
-          <i :class="obi.menuicon" v-if="obi.menuicon"></i>
           <span>{{$t(obi.name)}}</span>
+          <i class="el-icon-right"></i>
         </el-menu-item>
       </el-submenu>
       <el-menu-item v-else :index="ob.menuurl" v-show="ob.menuvisible">
-        <i :class="ob.menuicon" v-if="ob.menuicon"></i>
         <span>{{$t(ob.name)}}</span>
+        <i class="el-icon-right"></i>
       </el-menu-item>
       <!-- <div class="menu-footer" @click="isCollapse=!isCollapse">
         <div class="menu-trigger" :style="{transform:`rotateZ(${deg})`}">
@@ -46,92 +46,92 @@
 </template>
 
 <script>
-export default {
-  name: "vertical",
-  components: {},
-  data() {
-    return {
-      Index:"/index"
-    };
-  },
-  computed: {
-    deg() {
-      return this.isCollapse === false ? "0deg" : "180deg";
-    }
-  },
-  props: {
-    isCollapse: {
-      type: Boolean,
-      default: false
+  export default {
+    name: "vertical",
+    components: {},
+    data() {
+      return {
+        Index:"/index"
+      };
     },
-    backgroundColor: {
-      type: String,
-      default: "#ffffff"
-    },
-    textColor: {
-      type: String,
-      default: "#303133"
-    },
-    activeTextColor: {
-      type: String,
-      default: "#005BAA"
-    },
-    data: {
-      type: Array,
-      default: function () {
-        return [
-        ];
+    computed: {
+      deg() {
+        return this.isCollapse === false ? "0deg" : "180deg";
       }
     },
-    handleSelect: {
-      type: Function,
-      default:function () {
+    props: {
+      isCollapse: {
+        type: Boolean,
+        default: false
+      },
+      backgroundColor: {
+        type: String,
+        default: "#ffffff"
+      },
+      textColor: {
+        type: String,
+        default: "#303133"
+      },
+      activeTextColor: {
+        type: String,
+        default: "#005BAA"
+      },
+      data: {
+        type: Array,
+        default: function () {
+          return [
+          ];
+        }
+      },
+      handleSelect: {
+        type: Function,
+        default:function () {
 
+        }
+      },
+      activeIndex:{
+        type:String,
+        default:"/index"
       }
     },
-    activeIndex:{
-      type:String,
-      default:"/index"
+    methods: {},
+    mounted() {
+    },
+    watch: {
+      activeIndex(val){
+        this.$nextTick(function () {
+          this.Index = val;
+          this.$router.push(val);
+        });
+      }
     }
-  },
-  methods: {},
-  mounted() {
-  },
-  watch: {
-    activeIndex(val){
-      this.$nextTick(function () {
-        this.Index = val;
-        this.$router.push(val);
-      });
-    }
-  }
-};
+  };
 </script>
 <style lang='scss' scoped>
-.menu-footer {
-  height: 48px;
-  line-height: 48px;
-}
+  .menu-footer {
+    height: 48px;
+    line-height: 48px;
+  }
 
-.menu-trigger {
-  cursor: pointer;
-  width: 90%;
-  margin: 0 auto;
-  text-align: center;
-}
-.el-menu-item{
-  height: 30px;
-  line-height: 30px;
-  min-width: 120px;
-  max-width: 200px;
-  overflow-x: hidden;
-  overflow-y:hidden;
-  margin-left: 20px;
-}
-/deep/ .el-submenu__title{
-  height: 30px;
-  line-height: 30px;
-  background-color:#7ACAFF;
-  border: black 1px solid;
-}
+  .menu-trigger {
+    cursor: pointer;
+    width: 90%;
+    margin: 0 auto;
+    text-align: center;
+  }
+  .el-menu-item{
+    height: 38px;
+    line-height: 38px;
+    min-width: 120px;
+    max-width: 200px;
+    overflow-x: hidden;
+    overflow-y:hidden;
+    border: black 1px solid;
+  }
+  /deep/ .el-submenu__title{
+    background-image: linear-gradient(to right, #005BAA , white);
+    height: 38px;
+    line-height: 38px;
+    border: black 1px solid;
+  }
 </style>
