b05aeb60e796a6c3cfdf83728c8304301e7317d0
刘德越 <9831848145@163.COM>
2020-02-27 17:30:33 +0800

    国际化+路由

--- src/utils/menu.js
+++ src/utils/menu.js
@@ -525,6 +525,20 @@ export function menu() {
                   'menuicon': '',
                   'children': [
                     {
+                      '_id': 'PFANS1029',
+                      'name': 'menu.PFANS1029',
+                      'menuvisible': true,
+                      'menuurl': '/PFANS1029View',
+                      'menuicon': '',
+                      'children': [],
+                      'actions': [
+                        {"_id": "PFANS1029_read", "actiontype": "0", "name": 'button.view', "auth": 4},
+                        {"_id": "PFANS1029_new", "actiontype": "new", "name": 'button.insert', "auth": 4},
+                        {"_id": "PFANS1029_edit", "actiontype": "edit", "name": 'button.update', "auth": 4},
+                        {"_id": "PFANS1029_del", "actiontype": "del", "name": 'button.delete', "auth": 4}
+                      ]
+                    },
+                    {
                       '_id': 'PFANS1028',
                       'name': 'menu.PFANS1028',
                       'menuvisible': true,
