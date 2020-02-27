6d852db7c9f7623ce8d8d48db00945fff48faad3
赵国栋 <guodong.zhao@newtouch.cn>
2020-02-27 16:41:36 +0800

    删除回到顶部

--- src/components/EasyNormalContainer/index.vue
+++ src/components/EasyNormalContainer/index.vue
@@ -16,24 +16,7 @@
       <div align="right" class="filter-container" style="padding-bottom: 1rem">
         <span class="Title_front main_color" style="float:left">{{$t(title)}}</span>
       </div>
-
       <slot name="customize"></slot>
-      <el-backtop>
-        <div
-          style="{
-        height: 100%;
-        width: 100%;
-        background-color: #f2f5f6;
-        box-shadow: 0 0 6px rgba(0,0,0, .12);
-        text-align: center;
-        line-height: 40px;
-        color: #1989fa;
-      }"
-        >
-          UP
-        </div>
-      </el-backtop>
-
     </el-card>
   </div>
 
