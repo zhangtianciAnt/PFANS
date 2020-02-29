e8d60c8c78fb2356632824a6e91270ed4188a8c9
高兵兵 <bingbing.gao@newtouch.com>
2020-02-29 11:12:39 +0800

    销售TM添加

---
+++ src/view/PFANS/PFANS1000/PFANS1040/PFANS1040Api.js
@@ -0,0 +1,38 @@
+import request from '../../../../utils/request'
+
+export function get() {
+  return request({
+    url: 'contracttheme/get',
+    method: 'get'
+  })
+}
+
+export function One(data) {
+  return request({
+    url: 'contracttheme/one',
+    method: 'post',
+    data: data
+  })
+}
+
+export function update(data) {
+  return request({
+    url: 'contracttheme/update',
+    method: 'post',
+    data: data
+  })
+}
+
+export function insert(data) {
+  return request({
+    url: 'contracttheme/insert',
+    method: 'post',
+    data: data
+  })
+}
+
+
+
+
+
+
---
+++ src/view/PFANS/PFANS1000/PFANS1040/PFANS1040Store.js
@@ -0,0 +1,69 @@
+import {
+  insert,
+  get,
+  One,
+  update
+}
+  from './PFANS1040Api'
+
+const PFANS1039Store = {
+  namespaced: true,
+  state: {},
+  mutations: {},
+  actions: {
+    get() {
+      return new Promise((resolve, reject) => {
+        get().then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+    One({commit}, data) {
+      return new Promise((resolve, reject) => {
+        One(data).then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+    update({commit}, data) {
+      return new Promise((resolve, reject) => {
+        update(data).then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+    insert({commit}, data) {
+      return new Promise((resolve, reject) => {
+        insert(data).then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    }
+  }
+}
+
+export default PFANS1040Store;
