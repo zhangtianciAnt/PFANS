518bc34220e54585609fa8bac4f03cf2fa944fb5
费家亮 <jialiang.fei@newtouch.com>
2020-03-04 17:17:56 +0800

    添加生成按钮

--- src/view/PFANS/PFANS1000/PFANS1025/PFANS1025Api.js
+++ src/view/PFANS/PFANS1000/PFANS1025/PFANS1025Api.js
@@ -1,4 +1,5 @@
 import request from '../../../../utils/request'
+import requestDownload from '../../../../utils/requestDownload';
 
 export function get(data) {
   return request({
@@ -19,5 +20,13 @@ export function selectById(data) {
     url: 'award/selectById',
     method: 'get',
     params: data
+  })
+}
+export function generateJxls(data) {
+  return requestDownload({
+    url: 'award/generateJxls',
+    method: 'post',
+    data: data,
+    responseType: 'blob'
   })
 }
