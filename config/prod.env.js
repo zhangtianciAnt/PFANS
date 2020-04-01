42e74b3ad0d9180ab0e14f87cca73904c59f199b
赵国栋 <guodong.zhao@newtouch.cn>
2020-04-01 09:12:19 +0800

    发布地址变化

--- config/prod.env.js
+++ config/prod.env.js
@@ -8,8 +8,8 @@ module.exports = merge(prodEnv, {
   //正式环境
   // BASE_API: '"http://10.194.144.208:5556/"',
   // WS_API: '"ws://10.194.144.208:5556/"',
-  // UPLOAD_URL:'"http://10.194.144.208:8002/kodexplorer/?explorer/fileUpload&accessToken={1}&upload_to=E:/Kodkit/root/data/User/admin/home/PFANS/"',
-  // DOWN_URL:'"http://10.194.144.208:8002/kodexplorer/?explorer/fileDownload&accessToken={1}"'
+  // UPLOAD_URL:'"http://10.194.144.208:8002/?explorer/fileUpload&accessToken={1}&upload_to=E:/Kodkit/root/data/User/admin/home/PFANS/"',
+  // DOWN_URL:'"http://10.194.144.208:8002/?explorer/fileDownload&accessToken={1}"'
 
   // 预生成环境
   BASE_API:'"http://59.80.34.104:5556/"',
