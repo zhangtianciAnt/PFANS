e3f4e541e0f88f43f5f1a4b9ec54e31a05456177
高兵兵 <bingbing.gao@newtouch.com>
2020-04-02 19:04:18 +0800

    异常考勤二次审批修改

--- src/view/PFANS/PFANS2000/PFANS2016/PFANS2016FormView.vue
+++ src/view/PFANS/PFANS2000/PFANS2016/PFANS2016FormView.vue
@@ -755,10 +755,10 @@
                             this.workflowCode = 'W0003';
                             this.canStart = true;
                         } else if (this.form.status === '4') {
-                            this.workflowCode = 'W0056';
+                            this.workflowCode = 'W0059';
                             this.canStart = true;
                         } else if (this.form.status === '7') {
-                            this.workflowCode = 'W0056';
+                            this.workflowCode = 'W0059';
                             this.canStart = false;
                         }
                         this.loading = false;
@@ -1707,13 +1707,13 @@
                 if (val.state === '1') {
                     if (val.workflowCode === 'W0003') {
                         this.form.status = '3';
-                    } else if (val.workflowCode === 'W0056') {
+                    } else if (val.workflowCode === 'W0059') {
                         this.form.status = '6';
                     }
                 } else if (val.state === '2') {
                     if (val.workflowCode === 'W0003') {
                         this.form.status = '4';
-                    } else if (val.workflowCode === 'W0056') {
+                    } else if (val.workflowCode === 'W0059') {
                         this.form.status = '7';
                         this.canStart = false;
                     }
