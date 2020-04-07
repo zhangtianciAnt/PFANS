977e544f1b89e6c9c3ab9e9fb8c99783558fca6d
高兵兵 <bingbing.gao@newtouch.com>
2020-04-07 15:26:24 +0800

    考勤时间按照【0.01】添加

--- src/view/PFANS/PFANS1000/PFANS1026/PFANS1026FormView.vue
+++ src/view/PFANS/PFANS1000/PFANS1026/PFANS1026FormView.vue
@@ -1922,20 +1922,11 @@
           entrycondition = letentrycondition.value2;
         }
         //契約書番号(契約種類 + 事業年度 + 上下期 + 社内組織番号)
-        //通し番号
-        let number = '0001';
-        if (this.contractnumbercount.toString().length === 1) {
-          number = '000' + this.contractnumbercount;
-        } else if (this.contractnumbercount.toString().length === 2) {
-          number = '00' + this.contractnumbercount;
-        } else if (this.contractnumbercount.toString().length === 3) {
-          number = '0' + this.contractnumbercount;
-        }
         if (this.checked) {
           this.letcontractnumber = this.form.contractnumber.split('-')[0] + letbook;
         } else {
           if(this.groupinfo[2] !== null){
-            this.letcontractnumber = abbreviation + applicationdate + entrycondition + this.groupinfo[2] + number + letbook;
+            this.letcontractnumber = abbreviation + applicationdate + entrycondition + this.groupinfo[2] + letbook;
           } else {
             Message({
               message: this.$t("normal.error_14"),
