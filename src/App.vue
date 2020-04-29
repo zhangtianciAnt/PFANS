759e79cf6472e62ca7ca174728d6f9b7ba7d9528
赵国栋 <guodong.zhao@newtouch.cn>
2020-04-29 13:19:59 +0800

    屏蔽

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
       var e = event || window.event || arguments.callee.caller.arguments[0];
 
       if (e && e.keyCode == 123) {
-        alert("好好工作！！！");
+        alert("不要看我，我会害羞！~");
         e.returnValue = false;
         return (false);
       }
@@ -24,7 +24,7 @@ export default {
       var e = event || window.event || arguments.callee.caller.arguments[0];
 
       if (e && e.keyCode == 123) {
-        alert("好好工作！！！");
+        alert("不要看我，我会害羞！~");
         e.returnValue = false;
         return (false);
       }
@@ -33,7 +33,7 @@ export default {
       var e = event || window.event || arguments.callee.caller.arguments[0];
 
       if (e && e.keyCode == 123) {
-        alert("好好工作！！！");
+        alert("不要看我，我会害羞！~");
         e.returnValue = false;
         return (false);
       }
@@ -49,7 +49,7 @@ export default {
 
     mAlert() {
 
-      alert("好好工作！！！");
+      alert("不要看我，我会害羞！~");
     }
   }
 }
