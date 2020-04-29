19f6f17d7da044993f82337ee86568d0e974f3b3
赵国栋 <guodong.zhao@newtouch.cn>
2020-04-29 10:39:55 +0800

    屏蔽

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="theme_panasonic">
+  <div class="theme_panasonic" @mousedown="mousedown($event)">
     <transition name="el-fade-in">
     <router-view style="font-family: 'Helvetica Neue', 'Arial'"/>
     </transition>
@@ -8,7 +8,50 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  created() {
+    document.onkeydown = function(event) {
+      debugger
+      var e = event || window.event || arguments.callee.caller.arguments[0];
+
+      if (e && e.keyCode == 123) {
+        alert("小朋友你是否有很多问好？？？？");
+        e.returnValue = false;
+        return (false);
+      }
+    },
+    document.onkeyup = function(event) {
+      var e = event || window.event || arguments.callee.caller.arguments[0];
+
+      if (e && e.keyCode == 123) {
+        alert("小朋友你是否有很多问好？？？？");
+        e.returnValue = false;
+        return (false);
+      }
+    },
+    document.onkeypress = function(event) {
+      var e = event || window.event || arguments.callee.caller.arguments[0];
+
+      if (e && e.keyCode == 123) {
+        alert("小朋友你是否有很多问好？？？？");
+        e.returnValue = false;
+        return (false);
+      }
+    }
+  },
+  methods: {
+    mousedown :function mdClick(event) {
+      var e = event || window.event || arguments.callee.caller.arguments[0];
+      if (e.button == 2 || e.button == 3) {
+        this.mAlert();
+      }
+    },
+
+    mAlert() {
+
+      alert("小朋友你是否有很多问好？？？？");
+    }
+  }
 }
 </script>
 <style>
