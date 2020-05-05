dfe3e89220d3e571ca1f624222f242dff9975ea3
赵国栋 <guodong.zhao@newtouch.cn>
2020-05-05 15:02:38 +0800

    屏蔽

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,64 @@
 <template>
-  <div class="theme_panasonic" @mousedown="mousedown($event)">
+  <div @mousedown="mousedown($event)" class="theme_panasonic">
     <transition name="el-fade-in">
-    <router-view style="font-family: 'Helvetica Neue', 'Arial'"/>
+      <router-view style="font-family: 'Helvetica Neue', 'Arial'"/>
     </transition>
   </div>
 </template>
 
 <script>
 
-import { Message } from 'element-ui';
+  import {Message} from 'element-ui';
 
-export default {
-  name: 'App',
-  created() {
-    document.onkeydown = function(event) {
-      var e = event || window.event || arguments.callee.caller.arguments[0];
+  export default {
+    name: 'App',
+    created() {
+      document.onkeydown = function (event) {
+        var e = event || window.event || arguments.callee.caller.arguments[0];
 
-      if (e && e.keyCode == 123) {
-        Message({
-          message: '👀~再看就把你吃掉~'
-        });
-        e.returnValue = false;
-        return (false);
-      }
-    },
-    document.onkeyup = function(event) {
-      var e = event || window.event || arguments.callee.caller.arguments[0];
+        if (e && e.keyCode == 123) {
+          Message({
+            message: '👀~再看就把你吃掉~'
+          });
+          e.returnValue = false;
+          return (false);
+        }
+      },
+        document.onkeyup = function (event) {
+          var e = event || window.event || arguments.callee.caller.arguments[0];
 
-      if (e && e.keyCode == 123) {
-        // Message({
-        //   message: '👀~再看就把你吃掉~',
-        //   type: 'warning'
-        // });
-        e.returnValue = false;
-        return (false);
-      }
-    },
-    document.onkeypress = function(event) {
-      var e = event || window.event || arguments.callee.caller.arguments[0];
+          if (e && e.keyCode == 123) {
+            // Message({
+            //   message: '👀~再看就把你吃掉~',
+            //   type: 'warning'
+            // });
+            e.returnValue = false;
+            return (false);
+          }
+        },
+        document.onkeypress = function (event) {
+          var e = event || window.event || arguments.callee.caller.arguments[0];
 
-      if (e && e.keyCode == 123) {
-        Message({
-          message: '👀~再看就把你吃掉~'
-        });
-        e.returnValue = false;
-        return (false);
-      }
+          if (e && e.keyCode == 123) {
+            Message({
+              message: '👀~再看就把你吃掉~'
+            });
+            e.returnValue = false;
+            return (false);
+          }
+        },
+        document.oncontextmenu = function (event) {
+          var e = event || window.event || arguments.callee.caller.arguments[0];
+          if (e.button == 2 || e.button == 3) {
+            Message({
+              message: '👀~再看就把你吃掉~'
+            });
+            e.returnValue = false;
+            return (false);
+          }
+        }
     }
-  },
-  methods: {
-    // mousedown :function mdClick(event) {
-    //   var e = event || window.event || arguments.callee.caller.arguments[0];
-    //   if (e.button == 2 || e.button == 3) {
-    //     this.mAlert();
-    //   }
-    // },
-    //
-    // mAlert() {
-    //   alert("👀~再看就把你吃掉~");
-    // }
   }
-}
 </script>
 <style>
   #nprogress .bar {
