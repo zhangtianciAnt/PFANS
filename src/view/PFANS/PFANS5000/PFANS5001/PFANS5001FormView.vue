89652c71da18c102c8a188d169ed57ccc73bbf17
赵国栋 <guodong.zhao@newtouch.cn>
2020-05-21 13:24:41 +0800

    项目类型为自主投资时，合同非必填项

--- src/view/PFANS/PFANS5000/PFANS5001/PFANS5001FormView.vue
+++ src/view/PFANS/PFANS5000/PFANS5001/PFANS5001FormView.vue
@@ -3049,7 +3049,7 @@
                 type: 'error',
                 duration: 5 * 1000,
               });
-            } else if (error3 != 0 && this.form.toolstype !== '0') {
+            } else if (error3 != 0 && this.form.toolstype !== '0' && this.form.projecttype !== 'PP001006') {
               this.activeName = 'fifth';
               this.loading = false;
               Message({
