253eec771074f9fabbf2d06b57470665fec2d4e6
高兵兵 <bingbing.gao@newtouch.com>
2020-06-10 19:34:02 +0800

    BP添加管理员

--- src/view/PFANS/PFANS6000/PFANS6009/PFANS6009View.vue
+++ src/view/PFANS/PFANS6000/PFANS6009/PFANS6009View.vue
@@ -1542,41 +1542,43 @@
         this.loading = true;
         let role = getCurrentRole();
         const vote = [];
-        if (role === '3') {
-          vote.push(
-            {
-              value: this.$store.getters.userinfo.userinfo.groupid,
-              lable: this.$store.getters.userinfo.userinfo.groupname,
-            },
-          );
-        } else if (role === '2') {
-          let centerId = this.$store.getters.userinfo.userinfo.centerid;
-          let orgs = getDownOrgInfo(centerId);
-          if (orgs) {
-            for (let org of orgs) {
-              vote.push(
-                {
-                  value: org._id,
-                  lable: org.companyname,
-                },
-              );
+        if(this.$store.getters.useraccount._id != '5e78b17ef3c8d71e98a2aa30'){
+          if (role === '3') {
+            vote.push(
+              {
+                value: this.$store.getters.userinfo.userinfo.groupid,
+                lable: this.$store.getters.userinfo.userinfo.groupname,
+              },
+            );
+          } else if (role === '2') {
+            let centerId = this.$store.getters.userinfo.userinfo.centerid;
+            let orgs = getDownOrgInfo(centerId);
+            if (orgs) {
+              for (let org of orgs) {
+                vote.push(
+                  {
+                    value: org._id,
+                    lable: org.companyname,
+                  },
+                );
+              }
             }
-          }
 
-        } else if (role === '1') {
-          let centerId = this.$store.getters.userinfo.userinfo.centerid;
-          let orgs = getDownOrgInfo(centerId);
-          if (orgs) {
-            for (let center of orgs) {
-              let centers = getDownOrgInfo(center._id);
-              if (centers) {
-                for (let group of centers) {
-                  vote.push(
-                    {
-                      value: group._id,
-                      lable: group.companyname,
-                    },
-                  );
+          } else if (role === '1') {
+            let centerId = this.$store.getters.userinfo.userinfo.centerid;
+            let orgs = getDownOrgInfo(centerId);
+            if (orgs) {
+              for (let center of orgs) {
+                let centers = getDownOrgInfo(center._id);
+                if (centers) {
+                  for (let group of centers) {
+                    vote.push(
+                      {
+                        value: group._id,
+                        lable: group.companyname,
+                      },
+                    );
+                  }
                 }
               }
             }
@@ -1584,6 +1586,7 @@
         }
         const vote1 = [];
         if (this.$store.getters.userinfo.userid === '5e78fefff1560b363cdd6db7'
+          || this.$store.getters.useraccount._id === '5e78b17ef3c8d71e98a2aa30'
           || this.$store.getters.userinfo.userid === '5e78b2254e3b194874180f31'
           || this.$store.getters.userinfo.userid === '5e78b2004e3b194874180e21'
           || this.$store.getters.userinfo.userid === '5e78b2064e3b194874180e4d') {
