c1cb9385b82c8e559c3cf15aa2a3b619c1059239
wangshuai <1203765575@qq.com>
2020-07-03 16:06:57 +0800

    出差精算修改

--- src/view/PFANS/PFANS1000/PFANS1013/PFANS1013FormView.vue
+++ src/view/PFANS/PFANS1000/PFANS1013/PFANS1013FormView.vue
@@ -1490,6 +1490,12 @@
                   this.newaccountcodeflg1 = acinfo.code;
                   this.newsubjectnumberflg = acinfo.value2;
                 }
+                // add_fjl --获取住宿费的科目代码
+                let accountinf0 = getDictionaryInfo(this.newaccountcodeflg1);
+                if (accountinf0) {
+                  this.accflg = accountinf0.value2;
+                }
+                // add_fjl --获取住宿费的科目代码
                 // this.tableA[i].code20 = '';
                 //PL摘要内容
                 let plsuinfo = getDictionaryInfo(this.tableA[i].plsummary);
@@ -2252,56 +2258,41 @@
         }
       },
       gettype(val) {
-        // [{
-        //   evectionid: '',
-        //   accommodationdetails_id: '',
-        //   accommodationdate: [],
-        //   invoicenumber: '',
-        //   plsummary: this.plsummaryflg,
-        //   accountcode: this.Redirict === '0' ? 'PJ119001' : 'PJ132001',
-        //   budgetcoding: this.encoding,
-        //   subjectnumber: '',
-        //   departmentname: this.groupId,
-        //   activitycontent: '',
-        //   city: '',
-        //   region: '',
-        //   facilitytype: '',
-        //   facilityname: '',
-        //   currency: '',
-        //   // accommodation: '',
-        //   rmb: '',
-        //   travel: '',
-        //   annexno: '',
-        //   rowindex: '',
-        //   taxes: '',
-        //   // costitem: '',
-        // },
-        //   {
-        //     evectionid: '',
-        //     accommodationdetails_id: '',
-        //     accommodationdate: [],
-        //     invoicenumber: '',
-        //     plsummary: this.plsummaryflg,
-        //     accountcode: this.Redirict === '0' ? 'PJ119005' : 'PJ132005',
-        //     budgetcoding: this.encoding,
-        //     subjectnumber: '',
-        //     departmentname: this.groupId,
-        //     activitycontent: '',
-        //     city: '',
-        //     region: '',
-        //     facilitytype: '',
-        //     facilityname: '',
-        //     currency: '',
-        //     // accommodation: '',
-        //     rmb: '',
-        //     travel: '',
-        //     annexno: '',
-        //     rowindex: '',
-        //     taxes: '',
-        //     // costitem: '',
-        //   }],
+        this.tableT = [{
+          budgetcoding: '',
+          trafficdate: '',
+          invoicenumber: '',
+          departmentname: '',
+          plsummary: this.plsummaryflg,
+          accountcode: this.accountcodeflg,
+          subjectnumber: this.subjectnumberflg,
+          taxes: '',
+          // costitem: '',
+          region: '',
+          vehicle: '',
+          startingpoint: '',
+          rmb: '',
+          // taxrate: '',
+          currency: '',
+          foreigncurrency: '',
+          annexno: '',
+          rowindex: '',
+        }];
+        this.tableR = [{
+          otherdetailsdate: '',
+          // costitem: '',
+          plsummary: this.plsummaryflg,
+          accountcode: this.oldaccountcodeflg,
+          rmb: '',
+          subjectnumber: this.oldsubjectnumberflg,
+          remarks: '',
+          budgetcoding: '',
+          foreigncurrency: '',
+          annexno: '',
+          currency: '',
+        }];
         this.activeName = 'first',
-          this.form.type = val;
+        this.form.type = val;
         this.code21 = this.form.type == '0' ? 'PJ036' : 'PJ017';
         this.relations = [];
         if (val === '0') {
