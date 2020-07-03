37e4eaf634839e27bd85759b5e39aff20431113e
wangshuai <1203765575@qq.com>
2020-07-03 10:54:09 +0800

    代码优化

--- src/view/PFANS/PFANS1000/PFANS1031/PFANS1031FormView.vue
+++ src/view/PFANS/PFANS1000/PFANS1031/PFANS1031FormView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="min-height: 100%">`
+  <div style="min-height: 100%">
     <EasyNormalContainer :buttonList="buttonList" :title="title" @buttonClick="buttonClick" ref="container"
                          v-loading="loading" :canStart="canStart">
       <div slot="customize">
