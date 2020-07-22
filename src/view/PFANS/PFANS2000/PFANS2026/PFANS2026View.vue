99af2790e27b4ceb5c74734f2a9ece1efbd4ce2d
wangshuai <1203765575@qq.com>
2020-07-22 11:45:52 +0800

    离职修改

--- src/view/PFANS/PFANS2000/PFANS2026/PFANS2026View.vue
+++ src/view/PFANS/PFANS2000/PFANS2026/PFANS2026View.vue
@@ -288,7 +288,7 @@
           } else {
             this.checktype = 0;
           }
-          if (this.userid === row.user_id && row.newhope_exit_date != '') {
+          if (this.userid === row.user_id && row.newhope_exit_date != null) {
             this.buttonList[4].disabled = false;
           }
           this.status = 4;
