138fa52f945c13ce19c1676598602f6fc09e3581
wangshuai <1203765575@qq.com>
2020-08-07 14:07:51 +0800

    决裁书修正

--- src/view/PFANS/PFANS1000/PFANS1025/PFANS1025FormView.vue
+++ src/view/PFANS/PFANS1000/PFANS1025/PFANS1025FormView.vue
@@ -487,7 +487,7 @@
             <el-tab-pane :label="$t('label.PFANS2022VIEW_UPDATINGFILES')" name="thrid">
               <el-row>
                 <el-col :span="8">
-                  <el-form-item :label="$t('label.enclosure')"  :error="errorfile">
+                  <el-form-item :label="$t('label.enclosure')" :error="errorfile">
                     <el-upload
                       :action="upload"
                       :disabled="!disable"
@@ -535,6 +535,7 @@
     uploadUrl,
   } from '@/utils/customize';
   import EasyPop from '@/components/EasyPop';
+
   export default {
     name: 'PFANS1025FormView',
     components: {
@@ -808,8 +809,9 @@
               this.form.claimamount = mamount;
               this.userlist = this.form.user_id;
               // add-ws-7/17-禅道116任务
-
-              this.policycontractlist();
+              if (this.form.dates != null && this.form.dates != '') {
+                this.policycontractlist();
+              }
               // add-ws-7/17-禅道116任务
               this.baseInfo.award = JSON.parse(JSON.stringify(this.form));
               this.baseInfo.awardDetail = JSON.parse(JSON.stringify(this.tableT));
@@ -1379,7 +1381,7 @@
                           //add-ws-4/28-附件为空的情况下发起审批，提示填入必须项后程序没有终止修
                           if (val === 'StartWorkflow') {
                             this.$refs.container.$refs.workflow.startWorkflow();
-                          }else{
+                          } else {
                             this.paramsTitle();
                           }
                           //add-ws-4/28-附件为空的情况下发起审批，提示填入必须项后程序没有终止修改
@@ -1394,14 +1396,14 @@
                         });
                     }
                   } else {
-                    if(this.form.policycontract_id){
+                    if (this.form.policycontract_id) {
                       Message({
                         message: this.$t('label.PFANS1025VIEW_CHECKCYCEL'),
                         type: 'error',
                         duration: 5 * 1000,
                       });
                       this.loading = false;
-                    }else{
+                    } else {
                       if (this.$route.params._id) {     //郛冶ｾ�
                         this.$store
                           .dispatch('PFANS1025Store/update', this.baseInfo)
@@ -1416,7 +1418,7 @@
                             //add-ws-4/28-附件为空的情况下发起审批，提示填入必须项后程序没有终止修
                             if (val === 'StartWorkflow') {
                               this.$refs.container.$refs.workflow.startWorkflow();
-                            }else{
+                            } else {
                               this.paramsTitle();
                             }
                             //add-ws-4/28-附件为空的情况下发起审批，提示填入必须项后程序没有终止修改
