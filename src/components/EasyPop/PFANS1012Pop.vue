bbb5addfafa80066a8c4e6876b85de84e48fc803
fjl <fjl121206>
2020-08-11 11:28:02 +0800

    修改查看按钮pop显示,恒展开 还原

--- src/components/EasyPop/PFANS1012Pop.vue
+++ src/components/EasyPop/PFANS1012Pop.vue
@@ -8,58 +8,57 @@
 
 <script>
 
-  import PFANS1012FormView from '@/view/PFANS/PFANS1000/PFANS1012/PFANS1012FormView.vue';
+    import PFANS1012FormView from '@/view/PFANS/PFANS1000/PFANS1012/PFANS1012FormView.vue';
 
-  export default {
-    name: 'PFANS1012Pop',
-    components: {
-      PFANS1012FormView,
-    },
-    data() {
-      return {
-        open: false,
-        bkParams: {},
-      };
-    },
-    props: {
-      url: {
-        type: String,
-        default: '',
-      },
-      params: {
-        type: String,
-        default: {},
-      },
-    },
-    methods: {
-      close() {
-        for (let key in this.$route.params) {
-          this.$route.params[key] = '';
-        }
-        for (let key in this.bkParams) {
-          this.$route.params[key] = this.bkParams[key];
-        }
-        this.$refs.child.$refs.container.Pop = false;
-        this.bkParams = {};
-      },
-    },
-    watch: {
-      open(val) {
-        debugger
-        if (val) {
-          this.bkParams = [...this.$route.params];
-          for (let key in this.params) {
-            this.$route.params[key] = this.params[key];
-          }
-            if (this.$refs.child) {
-                this.$nextTick(function () {
-                    this.$refs.child.$refs.container.Pop = true;
-                });
-            }
-        }
-      },
-    },
-  };
+    export default {
+        name: 'PFANS1012Pop',
+        components: {
+            PFANS1012FormView,
+        },
+        data() {
+            return {
+                open: false,
+                bkParams: {},
+            };
+        },
+        props: {
+            url: {
+                type: String,
+                default: '',
+            },
+            params: {
+                type: String,
+                default: {},
+            },
+        },
+        methods: {
+            close() {
+                for (let key in this.$route.params) {
+                    this.$route.params[key] = '';
+                }
+                for (let key in this.bkParams) {
+                    this.$route.params[key] = this.bkParams[key];
+                }
+                this.$refs.child.$refs.container.Pop = false;
+                this.bkParams = {};
+            },
+        },
+        watch: {
+            open(val) {
+                debugger
+                if (val) {
+                    this.bkParams = [...this.$route.params];
+                    for (let key in this.params) {
+                        this.$route.params[key] = this.params[key];
+                    }
+                    this.$nextTick(function () {
+                        this.$refs.child.$refs.container.Pop = true;
+                    });
+
+                }
+            },
+        },
+    };
 </script>
 
 <style lang='scss'>
