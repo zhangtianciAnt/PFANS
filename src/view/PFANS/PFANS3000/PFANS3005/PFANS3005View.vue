fdd59d0f3ae66e0178f350875a42cdf78c0d0b0a
changchunmei <chunmei.chang@newtouch.com>
2020-09-09 15:10:46 +0800

    客户提的Bug修正

--- src/view/PFANS/PFANS3000/PFANS3005/PFANS3005View.vue
+++ src/view/PFANS/PFANS3000/PFANS3005/PFANS3005View.vue
@@ -769,7 +769,7 @@
               } else {
                 let jmsg = '';
                 for (let j = 0; j < response.length; j++) {
-                  jmsg += response.get(j).purnumbers + ' , ';
+                  jmsg += response[j].purnumbers + ' , ';
                 }
                 Message({
                   message: this.$t('label.PFANS3005VIEW_NUMBERS') + ' : ' + jmsg + ' ' + this.$t('label.PFANS3005VIEW_MSG1'),
