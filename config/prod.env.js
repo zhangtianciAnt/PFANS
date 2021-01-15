c11e3d7ca26453622ea4666a749ba7195113c1e4
张天赐 <tianci.zhao@newtouch.cn>
2021-01-15 17:10:10 +0800

    2021测试环境

--- config/prod.env.js
+++ config/prod.env.js
@@ -8,11 +8,18 @@ module.exports = merge(prodEnv, {
 //59.46.185.130  测试
 //10.194.144.208  正式
   NODE_ENV: '"production"',
+
+
   //正式环境
   BASE_API: '"http://10.194.144.208:5556/"',
   WS_API: '"ws://10.194.144.208:5556/"',
-  // UPLOAD_URL:'"http://10.194.144.208:8002/?explorer/fileUpload&accessToken={1}&upload_to=E:/Kodkit/root/data/User/admin/home/PFANS/"',
-  // DOWN_URL:'"http://10.194.144.208:8002/?explorer/fileDownload&accessToken={1}"'
   UPLOAD_URL:'"http://10.194.144.212:80/kodexplorer/?explorer/fileUpload&accessToken={1}&upload_to=/opt/lampp/htdocs/kodexplorer/data/User/admin/home/document/"',
   DOWN_URL:'"http://10.194.144.212:80/kodexplorer/?explorer/fileDownload&accessToken={1}"'
+
+
+  //2021端口测试环境
+  // BASE_API: '"http://59.46.185.130:5556/"',
+  // WS_API: '"ws://59.46.185.130:5556/"',
+  // UPLOAD_URL:'"http://59.46.185.130:5558?explorer/fileUpload&accessToken={1}&upload_to=C:/Kodkit/root/data/User/admin/home/"',
+  // DOWN_URL:'"http://59.46.185.130:5558?explorer/fileDownload&accessToken={1}"'
 })
