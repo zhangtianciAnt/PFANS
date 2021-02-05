9f7664763c036bb388514608edc764098a9927f8
高兵兵 <bingbing.gao@newtouch.com>
2021-02-05 14:55:10 +0800

    测试环境做成

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,8 +4,8 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://127.0.0.1:5556/"',
-  WS_API: '"ws://127.0.0.1:5556/"',
-  UPLOAD_URL:'"http://192.168.0.137:5558?explorer/fileUpload&accessToken={1}&upload_to=C:/Kodkit/root/data/User/admin/home/"',
-  DOWN_URL:'"http://192.168.0.137:5558?explorer/fileDownload&accessToken={1}"'
+  BASE_API: '"http://127.0.0.1:2220/"',
+  WS_API: '"ws://127.0.0.1:2220/"',
+  UPLOAD_URL:'"http://59.46.185.130:5558?explorer/fileUpload&accessToken={1}&upload_to=C:/Kodkit/root/data/User/admin/home/"',
+  DOWN_URL:'"http://59.46.185.130:5558?explorer/fileDownload&accessToken={1}"'
 })
