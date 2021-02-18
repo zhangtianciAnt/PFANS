ec8bc931ad1251812712997cc6a3049b16fd9b00
wangshuai <1203765575@qq.com>
2021-02-18 15:18:30 +0800

    PSDCD_PFANS_20201123_XQ_018

---
+++ src/view/PFANS/PFANS1000/PFANS1048/PFANS1048Store.js
@@ -0,0 +1,75 @@
+import {getprojects,insert,get,selectById,downloadExcel} from './PFANS1048Api'
+const PFANS1048store = {
+  namespaced: true,
+  state: {},
+  mutations: {},
+  actions: {
+    selectById({commit}, data) {
+      return new Promise((resolve, reject) => {
+        selectById(data).then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message);
+          }
+        }).catch(error => {
+          reject(error);
+        });
+      });
+    },
+    get({ commit }, data) {
+      return new Promise((resolve, reject) => {
+        get(data).then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+    getprojects({ commit }, data) {
+      return new Promise((resolve, reject) => {
+        getprojects(data).then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+    downloadExcel({commit}, data) {
+      return new Promise((resolve, reject) => {
+        downloadExcel(data).then(response => {
+          if ( response.type.indexOf("json") == -1 ) {
+            resolve(response)
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+    insert({ commit }, data) {
+      return new Promise((resolve, reject) => {
+        insert(data).then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+  }
+};
+
+export default PFANS1048store;
