b83da54135fd9f7ec065c0b58775ff73dbb74fa2
wangshuai <1203765575@qq.com>
2021-02-24 15:27:52 +0800

    PSDCD_PFANS_20201122_XQ_039

--- src/view/PFANS/PFANS1000/PFANS1042/PFANS1042Store.js
+++ src/view/PFANS/PFANS1000/PFANS1042/PFANS1042Store.js
@@ -2,16 +2,28 @@ import {
   getPltab,
   insert,
   getCostList,
-  getCostLast
+  getCostLast,
+  list
 } from './PFANS1042Api'
-
-
 
 const PFANS1042Store = {
   namespaced: true,
   state: {},
   mutations: {},
   actions: {
+    list() {
+      return new Promise((resolve, reject) => {
+        list().then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message);
+          }
+        }).catch(error => {
+          reject(error);
+        });
+      });
+    },
     getPltab({commit}, data) {
       return new Promise((resolve, reject) => {
         getPltab(data).then(response => {
