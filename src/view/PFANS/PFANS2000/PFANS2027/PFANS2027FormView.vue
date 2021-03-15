f4833390c26b96e296ca95319bb3294a96d5b0c8
秦浩然 <haoran.qin@newtouch.com>
2021-03-15 16:14:27 +0800

    需求：556 月度賞与考课 小bug

--- src/view/PFANS/PFANS2000/PFANS2027/PFANS2027FormView.vue
+++ src/view/PFANS/PFANS2000/PFANS2027/PFANS2027FormView.vue
@@ -36,20 +36,20 @@
                     use-virtual
                     v-if="isShow"
                   >
-<!--                    <plx-table-column-->
-<!--                      fixed="left"-->
-<!--                      width="50">-->
-<!--                      <template slot-scope="scope">-->
-<!--                        <el-checkbox-->
-<!--                          v-model="scope.row.dis"-->
-<!--                          :disabled="!disabled"-->
-<!--                          :key="index"-->
-<!--                          @change="getChecked(scope.row.dis,scope.$index)"-->
-<!--                          active-value="1"-->
-<!--                          inactive-value="0"-->
-<!--                        ></el-checkbox>-->
-<!--                      </template>-->
-<!--                    </plx-table-column>-->
+                    <!--                    <plx-table-column-->
+                    <!--                      fixed="left"-->
+                    <!--                      width="50">-->
+                    <!--                      <template slot-scope="scope">-->
+                    <!--                        <el-checkbox-->
+                    <!--                          v-model="scope.row.dis"-->
+                    <!--                          :disabled="!disabled"-->
+                    <!--                          :key="index"-->
+                    <!--                          @change="getChecked(scope.row.dis,scope.$index)"-->
+                    <!--                          active-value="1"-->
+                    <!--                          inactive-value="0"-->
+                    <!--                        ></el-checkbox>-->
+                    <!--                      </template>-->
+                    <!--                    </plx-table-column>-->
                     <plx-table-column
                       :label="$t('label.PFANS2027VIEW_NAME')"
                       prop="userName"
@@ -1073,14 +1073,18 @@
           if (scope.prize === "无") {
             return true;
           } else {
-            if (this.$route.params.evaluatenum === "一次評価" && scope.process === "1") {
-              return false;
-            } else if (this.$route.params.evaluatenum === "二次評価" && scope.process === "2") {
-              return false;
-            } else if (this.$route.params.evaluatenum === "最终評価" && scope.process === "3") {
+            if (this.$store.getters.userinfo.userid === "5e78fefff1560b363cdd6db7" && scope.process === "4") {
               return false;
             } else {
-              return true;
+              if (this.$route.params.evaluatenum === "一次評価" && scope.process === "1") {
+                return false;
+              } else if (this.$route.params.evaluatenum === "二次評価" && scope.process === "2") {
+                return false;
+              } else if (this.$route.params.evaluatenum === "最终評価" && scope.process === "3") {
+                return false;
+              } else {
+                return true;
+              }
             }
           }
         }
