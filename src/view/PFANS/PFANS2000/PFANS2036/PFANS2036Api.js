88797d4e42053e86a9f56ea05c10444224824151
张天赐 <tianci.zhao@newtouch.cn>
2021-03-18 10:13:43 +0800

    版本

---
+++ src/view/PFANS/PFANS2000/PFANS2036/PFANS2036Api.js
@@ -0,0 +1,88 @@
+import request from '../../../../utils/request'
+
+export function getGroupId(data) {
+  return request({
+    url: 'personalcost/getGroupId',
+    method: 'get',
+    params: data
+  })
+}
+
+export function getChangeRanks(data) {
+  return request({
+    url: 'personalcost/getChangeRanks',
+    method: 'get',
+    params: data
+  })
+}
+
+export function getPersonalCost(data) {
+  return request({
+    url: 'personalcost/getPersonalCost',
+    method: 'get',
+    params: data
+  })
+}
+
+export function gettableBm(data) {
+  return request({
+    url: 'personalcost/gettableBm',
+    method: 'get',
+    params: data
+  })
+}
+
+export function gettableGs(data) {
+  return request({
+    url: 'personalcost/gettableGs',
+    method: 'get',
+    params: data
+  })
+}
+
+export function gettableRb(data) {
+  return request({
+    url: 'personalcost/gettableRb',
+    method: 'get',
+    params: data
+  })
+}
+
+
+
+
+
+
+export function getYears(data) {
+  return request({
+    url: 'personalcost/getYears',
+    method: 'get',
+    data: data
+  })
+}
+
+export function insertPenalcost(data) {
+  return request({
+    url: 'personalcost/insertPenalcost',
+    method: 'get',
+    data: data
+  })
+}
+
+
+export function upPersonalCost(data) {
+  return request({
+    url: 'personalcost/upPersonalCost',
+    method: 'post',
+    data: data
+  })
+}
+
+export function getFuzzyQuery(data) {
+  return request({
+    url: 'personalcost/getFuzzyQuery',
+    method: 'get',
+    params: data
+  })
+}
+
