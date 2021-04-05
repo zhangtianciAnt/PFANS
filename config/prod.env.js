fdcc9a107e74644edb583ddf2870ca3b28edcb81
张天赐 <tianci.zhao@newtouch.cn>
2021-04-05 20:18:03 +0800

    运维文件

--- config/prod.env.js
+++ config/prod.env.js
@@ -11,8 +11,14 @@ module.exports = merge(prodEnv, {
   //正式环境
   BASE_API: '"http://10.194.144.208:5556/"',
   WS_API: '"ws://10.194.144.208:5556/"',
-  // UPLOAD_URL:'"http://10.194.144.208:8002/?explorer/fileUpload&accessToken={1}&upload_to=E:/Kodkit/root/data/User/admin/home/PFANS/"',
-  // DOWN_URL:'"http://10.194.144.208:8002/?explorer/fileDownload&accessToken={1}"'
-  UPLOAD_URL:'"http://59.46.185.130:5558?explorer/fileUpload&accessToken={1}&upload_to=C:/Kodkit/root/data/User/admin/home/"',
-  DOWN_URL:'"http://59.46.185.130:5558?explorer/fileDownload&accessToken={1}"'
+  UPLOAD_URL:'"http://10.194.144.212:80/kodexplorer/?explorer/fileUpload&accessToken={1}&upload_to=/opt/lampp/htdocs/kodexplorer/data/User/admin/home/document/"',
+  DOWN_URL:'"http://10.194.144.212:80/kodexplorer/?explorer/fileDownload&accessToken={1}"'
+
+
+  //2020端口测试环境
+  // BASE_API: '"http://59.46.185.130:20/"',
+  // WS_API: '"ws://59.46.185.130:20/"',
+  // UPLOAD_URL:'"http://59.46.185.130:5558?explorer/fileUpload&accessToken={1}&upload_to=C:/Kodkit/root/data/User/admin/home/"',
+  // DOWN_URL:'"http://59.46.185.130:5558?explorer/fileDownload&accessToken={1}"'
+
 })
