f6ce94d73fffb1f436a05f924e12c4af589e1635
sunchengchuan <chengchuan.sun@newtouch.cn>
2021-06-11 10:15:41 +0800

    PSDCD_PFANS_设备管理部门选择下拉框，'全部'为首选项 scc 2021.6.11

--- src/view/ASSETS/ASSETS1000/ASSETS1001/ASSETS1001View.vue
+++ src/view/ASSETS/ASSETS1000/ASSETS1001/ASSETS1001View.vue
@@ -474,7 +474,11 @@
               }, []);
               for (let i = 0; i < filtersrst.length; i++) {
                 if (filtersrst[i].code == '' || filtersrst[i].code == null || filtersrst[i].code == undefined) {
-                  filtersrst[i].code = '全部';
+                  // let temp = "";
+                  //设备管理部门选择下拉框，'全部'为首选项 scc 2021.6.11
+                  filtersrst[i].code = filtersrst[i-1].code;
+                  filtersrst[i-1].code = '全部';
+                  // filtersrst[i].code = '全部';
                 }
               }
               this.options = filtersrst;
