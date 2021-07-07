114dc55b21f64d3a3d31469e5d10566c22d79db1
sunchengchuan <chengchuan.sun@newtouch.com>
2021-07-07 17:51:02 +0800

    重新修正编码显示

--- src/view/PFANS/PFANS8000/PFANS8009/PFANS8009FormView.vue
+++ src/view/PFANS/PFANS8000/PFANS8009/PFANS8009FormView.vue
@@ -217,6 +217,14 @@
           //   }
           // }
           // NT_PFANS_20210308_BUG_159  编码根据数据情况实时递增
+          let num = parseInt(this.letcode.substr(this.letcode.length-3,3));
+          if (num < 9) {
+            this.letcode = this.letcode.substring(0, 5) + "00" + (num - 1);
+          }else if(num === 10){
+            this.letcode = this.letcode.substring(0, 5) + "00" + (num - 1);
+          }else{
+            this.letcode = this.letcode.substring(0, 5) + "00" + (num - 1);
+          }
         } else {
           this.tableD = [
             {
