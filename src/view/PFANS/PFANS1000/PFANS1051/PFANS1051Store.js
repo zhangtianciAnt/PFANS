a2badaefdf2111f9acddf2910f2d0f4f9b19c0ec
changchunmei <chunmei.chang@newtouch.com>
2021-07-20 19:02:17 +0800

    部门别收支表前台

---
+++ src/view/PFANS/PFANS1000/PFANS1051/PFANS1051Store.js
@@ -0,0 +1,23 @@
+import {selectBygroupid} from './PFANS1051Api';
+const PFANS1051store = {
+  namespaced: true,
+  state: {},
+  mutations: {},
+  actions: {
+    selectBygroupid({commit}, data) {
+      return new Promise((resolve, reject) => {
+        selectBygroupid(data).then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message);
+          }
+        }).catch(error => {
+          reject(error);
+        });
+      });
+    },
+  },
+};
+
+export default PFANS1051store;
