0686b29ed7f94d85feddc0f68e33013bd851ca96
sunchengchuan <chengchuan.sun@newtouch.com>
2021-07-27 10:37:27 +0800

    123

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,7 +5,7 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   //本地
   NODE_ENV: '"development"',
-  BASE_API: '"http://127.0.0.1:5556/"',
+  BASE_API: '"http://127.0.0.1:5556/123"',
   WS_API: '"ws://127.0.0.1:5556/"',
   UPLOAD_URL:'"http://192.168.0.137:5558?explorer/fileUpload&accessToken={1}&upload_to=C:/Kodkit/root/data/User/admin/home/"',
   DOWN_URL:'"http://192.168.0.137:5558?explorer/fileDownload&accessToken={1}"'
