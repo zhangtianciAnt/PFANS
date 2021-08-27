6a41cd6df3073fecfaad42c6ec971735653bad03
sunchengchuan <chengchuan.sun@newtouch.com>
2021-08-27 09:38:57 +0800

    部门rank别人件费画面

---
+++ src/view/PFANS/PFANS4000/PFANS4003/PFANS4003Api.js
@@ -0,0 +1,23 @@
+import request from "../../../../utils/request"
+import requestDownload from "../../../../utils/requestDownload";
+
+
+export function getpeoplewareList(data) {
+  return request({
+    url: 'peopleware/getPeopleWare',
+    method: 'post',
+    data: data
+  })
+}
+export function download(data) {
+  return requestDownload({
+    url: 'peopleware/download',
+    method: 'get',
+    params: data,
+    responseType: 'blob'
+  })
+}
+
+
+
+
