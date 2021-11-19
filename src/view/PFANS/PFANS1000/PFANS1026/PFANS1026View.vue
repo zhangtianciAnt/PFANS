d413ec9337b1743468a8b48aa5afda9b4a09b0bc
sunchengchuan <chengchuan.sun@newtouch.com>
2021-11-19 18:33:54 +0800

    番号废弃，撤销后，按钮状态不可用

--- src/view/PFANS/PFANS1000/PFANS1026/PFANS1026View.vue
+++ src/view/PFANS/PFANS1000/PFANS1026/PFANS1026View.vue
@@ -323,6 +323,14 @@
             });
             return;
           }
+          //region scc add 一览获取数据状态到详细 from
+          let status = '';
+          if(this.rows.status === '未开始'){
+            status = '0';
+          }else if(this.rows.status === '驳回'){
+            status = '3';
+          }
+          //endregion scc add 一览获取数据状态到详细 to
           this.$router.push({
             name: 'PFANS1026FormView',
             params: {
@@ -330,6 +338,7 @@
               contractnumbercount: this.contractnumbercount,
               state: this.state,
               disabled: true,
+              _status: status,
             }
           })
         }
