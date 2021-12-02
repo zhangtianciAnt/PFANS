b32013171d038963e60e01adb65f209370e82605
molei <lei.mo@newtouch.com>
2021-12-02 14:28:26 +0800

    受托契约  请求书分页

--- src/view/PFANS/PFANS1000/PFANS1032/PFANS1032Store.js
+++ src/view/PFANS/PFANS1000/PFANS1032/PFANS1032Store.js
@@ -1,4 +1,5 @@
 import {
+  getPage,
   get,
   one,
   update,
@@ -23,6 +24,21 @@ const PFANS1032Store = {
         })
       })
     },
+    //  add  ml  211201  分页  from
+    getPage() {
+      return new Promise((resolve, reject) => {
+        getPage().then(response => {
+          if (response.code === 0) {
+            resolve(response.data);
+          } else {
+            reject(response.message)
+          }
+        }).catch(error => {
+          reject(error);
+        })
+      })
+    },
+    //  add  ml  211201  分页  to
     update({commit}, data) {
       return new Promise((resolve, reject) => {
         update(data).then(response => {
