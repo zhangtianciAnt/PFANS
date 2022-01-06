b82d09117a1ace98f5b2852fa84a122881b0ea51
张天赐 <tianci.zhao@newtouch.cn>
2022-01-06 13:51:33 +0800

    格式化代码test

--- src/components/EasyErrorPage/401.vue
+++ src/components/EasyErrorPage/401.vue
@@ -3,17 +3,18 @@
 </template>
 
 <script>
-  import error401 from '@/assets/jpg/403.jpg'
-  import errorContent from './error-content.vue'
-  export default {
-    name: 'error_401',
-    components: {
-      errorContent
-    },
-    data () {
-      return {
-        src: error401
-      }
+import error401 from '@/assets/jpg/403.jpg'
+import errorContent from './error-content.vue'
+
+export default {
+  name: 'error_401',
+  components: {
+    errorContent
+  },
+  data() {
+    return {
+      src: error401
     }
   }
+}
 </script>
